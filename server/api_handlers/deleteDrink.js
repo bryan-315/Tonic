2b46b61a405c98cb877c8fa71ac9aadd73766765
David Bryan Cruz Gumal <bryancruz420420@outlook.com>
2025-05-20 01:39:17 -0400

    finish deleteDrink, done with backend for now

--- server/api_handlers/deleteDrink.js
+++ server/api_handlers/deleteDrink.js
@@ -6,25 +6,35 @@ const deleteDrink = async (req, res) => {
         await client.connect();
         const db = client.db('tonic');
         const { drinkId } = req.params;
+        const { authorId } = req.body;
+        // Validate authorId
+        if (!authorId || !ObjectId.isValid(authorId)) {
+            return res.status(400).json({ status: 400, error: 'Invalid author ID' });
+        }
 
         // Validate drinkId
         if (!ObjectId.isValid(drinkId)) {
-            return res.status(400).json({ error: 'Invalid drink ID' });
+            return res.status(400).json({ status: 400, error: 'Invalid drink ID' });
         }
 
         // Check if the drink exists
-        const existingDrink = await db.collection('drinks').findOne({ _id: ObjectId(drinkId) });
+        const existingDrink = await db.collection('drinks').findOne({ _id: new ObjectId(drinkId) });
         if (!existingDrink) {
-            return res.status(404).json({ error: 'Drink not found' });
+            return res.status(404).json({ status: 404, error: 'Drink not found' });
+        }
+
+        // Check if the authorId matches the drink's authorId
+        if (existingDrink.createdBy.id.toString() !== authorId) {
+            return res.status(403).json({ status: 403, error: 'You are not authorized to delete this drink' });
         }
 
         // Delete the drink
-        await db.collection('drinks').deleteOne({ _id: ObjectId(drinkId) });
+        await db.collection('drinks').deleteOne({ _id: new ObjectId(drinkId) });
 
         return res.status(200).json({ status: 200, message: 'Drink deleted successfully' });
     } catch (err) {
         console.error('Error connecting to the database', err);
-        return res.status(500).json({ error: 'Internal server error' });
+        return res.status(500).json({ status: 500, error: 'Internal server error' });
     } finally {
         await client.close();
     }
